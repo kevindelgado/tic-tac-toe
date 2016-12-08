a66fca03c8c169da11aa8ba3785da7fa0a10fbd3
Kevin Delgado <delgado1kevin@gmail.com>
2016-12-08 16:36:14 -0500

    Remove reg clash for cleaned up wincheck

--- tictactoe_mem_wincheck.s
+++ tictactoe_mem_wincheck.s
@@ -491,7 +491,7 @@ win_helper:
 	nop
 	nop
 	nop
-	lw $14, 35($2)
+	lw $23, 35($2)
 	nop
 	nop
 	nop
@@ -504,7 +504,7 @@ win_helper:
 	nop
 	nop
 	nop
-	blt $14, $11, win_valid
+	blt $23, $11, win_valid
 	nop
 	nop
 	nop
